a9eaff945745c528edd94692d13ab823cba1f86c
Juan David Rincon <juanrincon223@gmail.com>
2025-04-12 23:01:14 -0500

    Update 5 Random_forest.R

--- Scripts/5 Random_forest.R
+++ Scripts/5 Random_forest.R
@@ -31,30 +31,32 @@ setwd(path)  # Establecer el directorio de trabajo
 library(pacman)
 
 p_load(rio, # import/export data
-       tidyverse, # tidy-data
-       glmnet, # To implement regularization algorithms. 
-       caret, # Creating predictive models
-       scatterplot3d, # For 3D visualization
+       tidyverse, 
+       glmnet, 
+       caret, 
+       scatterplot3d, 
        plotly,
        car,
        doParallel, 
        skimr,
        corrplot, 
-       ggplot2)  # Visualización interactiva
+       ggplot2, 
+       PROC) 
 
 # --------------------------
 # 3. CARGA DE DATOS
 # --------------------------
+# Bases de entrenamiento y de testeo
+train_hogares <- readRDS("stores/train_hogares_full.Rds") 
+test_hogares <- readRDS("stores/test_hogares_full.Rds") 
 
-train_hogares <- readRDS("stores/train_hogares_full.Rds")  # Cargar datos de entrenamiento
-test_hogares <- readRDS("stores/test_hogares_full.Rds")  # Cargar datos de prueba
-
-train_hogares <- as.data.frame(train_hogares)  # Convertir a data.frame
-test_hogares <- as.data.frame(test_hogares)  # Convertir a data.frame
+train_hogares <- as.data.frame(train_hogares) 
+test_hogares <- as.data.frame(test_hogares) 
 
 # --------------------------
 # 5. Medir correlación entre variables (Optimizar variables)
 # --------------------------
+# Ver correlación de variables para entender relaciones entre estas
 numeric_vars <- train_hogares[sapply(train_hogares, is.numeric)]
 cor_matrix <- cor(numeric_vars, use = "complete.obs")
 
@@ -72,29 +74,34 @@ dev.off()
 # --------------------------
 # 6. Ajustes Random Forest
 # --------------------------
-
+#Semilla
 set.seed(123)
 
+#Si bien hay una base test, esta partición permite sacar métricas importantes del modelo
+#Test no tiene indicador de pobreza
 inTrain <- createDataPartition(
-  y = train_hogares$Pobre,## La variable dependiente u objetivo 
-  p = .7, ## Usamos 70%  de los datos en el conjunto de entrenamiento 
+  y = train_hogares$Pobre,
+  p = .7, # 70%  de los datos en el conjunto de entrenamiento 
   list = FALSE)
 
 train_muestra <- train_hogares[ inTrain,]
 test_muestra  <- train_hogares[-inTrain,]
 
-
+# Cambiar a factor Pobreza
 train_muestra$Pobre <- factor(train_muestra$Pobre, 
                               levels = c(0, 1),
                               labels = c("No_Pobre", "Pobre"))
+test_muestra$Pobre <- factor(test_muestra$Pobre, 
+                              levels = c(0, 1),
+                              labels = c("No_Pobre", "Pobre"))
 
+# Ajustes del modelo
 fiveStats <- function(...) {
   c(
     caret::twoClassSummary(...),
     caret::defaultSummary(...)
   )
 }
-
 
 fitControl <- trainControl(
   method = "cv",  # Validación cruzada
@@ -106,28 +113,24 @@ fitControl <- trainControl(
   sampling = "smote"
 )
 
+# Ajustes para modelo extenso
 tunegrid_rf <- expand.grid(
   mtry = c(2, 4, 6, 8, 10, 12, 16 , 20),
   splitrule = c("gini", "extratrees"),
   min.node.size = c(1, 5, 10, 20)
 )
 
+# Ajustes para modelo inicial o prueba
 tunegrid_rf_fast <- expand.grid(
   mtry = c(4, 8),
   splitrule = c("gini"),
   min.node.size = c(1, 10)
 )
-
-#tunegrid_rf_final <- expand.grid(
-#  mtry = c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20),  
-#  splitrule = c("gini", "extratrees"),           
-#min.node.size = c(1, 5, 10, 20, 30, 40)        
-#)
  
 # --------------------------
 # 7. Random Forest Prueba (Importancia Variables)
 # -------------------------- 
-
+# Random Forest inicial (Prueba)
 rforest <- train(Pobre ~ JH_Mujer + JH_Edad + JH_RSS_S +
                  JH_NEduc + JH_CotizaPension + JH_Oc + JH_NoSeguro + Hijos + Educ_prom + 
                  JH_Independiente + JH_Microempresa +JH_Pequena  + JH_Mediana_Grande + Adultos + Trabajadores + Subsidios + Pensionado + JH_Trabaja +JH_HorasExt +
@@ -144,6 +147,7 @@ rforest <- train(Pobre ~ JH_Mujer + JH_Edad + JH_RSS_S +
 # --------------------------
 # 8. Seleccionar Variables modelo final (Importancia)
 # -------------------------- 
+# Importancia de variables en predicción
 varImpPlot <- varImp(rforest)
 plot(varImpPlot, top = 20) 
 
@@ -151,9 +155,48 @@ jpeg("importancia_variables.jpg", width = 800, height = 600)
 plot(varImpPlot, top = 20, main = "Importancia de variables")
 dev.off()
 
+# Mejor combinación hiperparámetros
+print(rforest$bestTune)
+print(rforest$results)
+
+#Matriz de Confusión
+prediccion_test <- predict(rforest, newdata = test_muestra)
+cm <- confusionMatrix(prediccion_test, test_muestra$Pobre, positive = "Pobre")
+
+tabla_confusion <- as.table(cm$table)
+tabla_confusion_df <- as.data.frame(cm$table)
+xtable(tabla_confusion_df)
+
+ggplot(tabla_confusion_df, aes(x = Reference, y = Prediction)) +
+  geom_tile(aes(fill = Freq), color = "white") +
+  geom_text(aes(label = Freq), vjust = 1.5, size = 6, color = "white") +
+  scale_fill_gradient(low = "#6baed6", high = "#08306b") +
+  theme_minimal(base_size = 15) +
+  labs(title = "Matriz de Confusión", x = "Valor Real", y = "Valor Predicho")
+
+
+ggsave("matriz_confusion.jpeg", width = 8, height = 6, dpi = 300)
+
+# Tabla de resultado 
+accuracy <- cm$overall["Accuracy"]
+precision <- cm$byClass["Precision"]
+recall <- cm$byClass["Sensitivity"]      
+specificity <- cm$byClass["Specificity"]
+f1 <- cm$byClass["F1"]
+
+print(tabla_metricas)
+tabla_metricas <- data.frame(
+  Métrica = c("Accuracy", "Sensibilidad (Recall)", "Especificidad", "Precisión", "F1-Score"),
+  Valor = c(accuracy, recall, specificity, precision, f1)
+)
+xtable(tabla_metricas, digits = 4)
+
+
 # --------------------------
 # 8. Random Forest Final 
 # -------------------------- 
+
+# Random Forest real con ajustes para mejorar rendimiento
 cl <- makeCluster(detectCores() - 1)
 registerDoParallel(cl)
 rforest <- train(Pobre ~ JH_RSS_S + Hijos + Subsidios + Nper + Educ_prom + P5130 +
@@ -163,13 +206,18 @@ rforest <- train(Pobre ~ JH_RSS_S + Hijos + Subsidios + Nper + Educ_prom + P5130
                  trControl = fitControl,
                  tuneGrid = tunegrid_rf,
                  method = "ranger",
-                 ntree = 250,
+                 ntree = 300,
                  na.action = na.pass,
                  savePredictions = "final",
                  verbose = FALSE,
                  importance = "impurity")
 
+# Importancia final de las variables
 varImpPlot <- varImp(rforest)
+tabla_importancia <- data.frame(Variable = rownames(varImpPlot$importance),                                Importancia = varImpPlot$importance$Overall)
+xtable(tabla_importancia)
+
+print(tabla_latex, type = "latex")
 plot(varImpPlot, top = 20) 
 
 jpeg("importancia_variables_final.jpg", width = 800, height = 600)
@@ -183,55 +231,71 @@ registerDoSEQ()
 # --------------------------
 # 9. Métricas Importantes
 # -------------------------- 
-# Imprimir los mejores parámetros encontrados
+# Mejor combinación de hiperparámetros
 print(rforest$bestTune)
 print(rforest$results)
 
+# Base de predicción para Kaggle
 prediccion_test <- predict(rforest, newdata = test_hogares)
-
 predictSample <- data.frame(
   id = test_hogares$id, 
-  pobre = as.numeric(prediccion_test) - 1  # Convertir de factor a numérico (0 = No_Pobre, 1 = Pobre)
+  pobre = as.numeric(prediccion_test) - 1 
 )
-
-ruta_salida <- "predicciones_test_v4.csv"  
+ruta_salida <- "predicciones_test_v5.csv"  
 write.csv(predictSample, ruta_salida, row.names = FALSE)
 
 # --------------------------
 # 10. Análisis Adicionales
 # --------------------------
-prediccion_test <- predict(rforest, newdata = test_muestra)
 
-cm <- confusionMatrix(prediccion_test, test_muestra$Pobre, positive = "1")
+# Matriz de Confusión
+prediccion_test <- predict(rforest, newdata = test_muestra)
+cm <- confusionMatrix(prediccion_test, test_muestra$Pobre, positive = "Pobre")
 
 tabla_confusion <- as.table(cm$table)
-metricas <- as.data.frame(t(cm$byClass))  # Recall, Precision, F1, etc.
-globales <- as.data.frame(t(cm$overall))  # Accuracy, Kappa, etc.
+tabla_confusion_df <- as.data.frame(cm$table)
+xtable(tabla_confusion_df)
 
-write.csv(as.data.frame(tabla_confusion), "tabla_confusion.csv", row.names = FALSE)
-write.csv(metricas, "metricas_por_clase.csv")
-write.csv(globales, "metricas_generales.csv")
-  
-ggplot(cm, aes(x = Actual, y = Predicted)) +
+ggplot(tabla_confusion_df, aes(x = Reference, y = Prediction)) +
   geom_tile(aes(fill = Freq), color = "white") +
   geom_text(aes(label = Freq), vjust = 1.5, size = 6, color = "white") +
   scale_fill_gradient(low = "#6baed6", high = "#08306b") +
   theme_minimal(base_size = 15) +
   labs(title = "Matriz de Confusión", x = "Valor Real", y = "Valor Predicho")
 
+
 ggsave("matriz_confusion.jpeg", width = 8, height = 6, dpi = 300)
 
+# Tabla de resultados FINAL
+accuracy <- cm$overall["Accuracy"]
+precision <- cm$byClass["Precision"]
+recall <- cm$byClass["Sensitivity"]   
+specificity <- cm$byClass["Specificity"]
+f1 <- cm$byClass["F1"]
+kaggle <- 0.6703
+print(tabla_metricas)
+
+tabla_metricas <- data.frame(
+  Métrica = c("Accuracy", "Sensibilidad (Recall)", "Especificidad", "Precisión", "F1-Score", "Kaggle"),
+  Valor = c(accuracy, recall, specificity, precision, f1, kaggle)
+)
+
+xtable(tabla_metricas, digits = 4)
+
 # --------------------------
-# 11. Curva ROC
+# 11. Curva ROC (Adicional)
 # --------------------------
-install.packages("pROC")
-library(pROC)
-prediccion_test <- predict(rforest, newdata = test_muestra$Pobre)
+
+# Análisis de AUC y curva ROC 
+prediccion_test <- predict(rforest, newdata = test_muestra, type = "prob")[,2]
+roc_curve <- roc(test_muestra$Pobre, prediccion_test)
 print(roc_curve)
 auc_value <- auc(roc_curve)
 print(paste("AUC: ", round(auc_value, 2)))
+jpeg("curva_ROC.jpg", width = 800, height = 600, quality = 100)
 plot(roc_curve, col = "blue", 
      main = paste("Curva ROC (AUC = ", round(auc_value, 2), ")", sep = ""), 
      lwd = 2)
+dev.off()
 
 
